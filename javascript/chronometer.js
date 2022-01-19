5d77f58bad42c6d2853c306a69908e68bbbcddbc
Larissa Ferreira <larissadantasaf@gmail.com>
2022-01-19 15:19:27 -0300

    lab-javascript-chronometer

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,30 +1,38 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    const number = Math.floor(this.currentTime / 60);
+    return number;
   }
 
   getSeconds() {
-    // ... your code goes here
+    const seconds = (this.currentTime % 60);
+    return seconds;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return `0${value}`;
+    } 
+    return `${value}`;
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    return this.currentTime = 0;
   }
 
   split() {
