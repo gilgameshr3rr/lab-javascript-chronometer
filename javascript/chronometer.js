7762b9c41dc72b9d70c21ec0d16e94af5e28762a
Larissa Ferreira <larissadantasaf@gmail.com>
2022-01-19 21:57:04 -0300

    add split method

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -36,7 +36,7 @@ class Chronometer {
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
 
